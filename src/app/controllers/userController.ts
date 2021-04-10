7ecfe8ab8ea2991ce425a7a0e5fddad693234e23
Warley Henrique <warley.henrique@gmail.com>
2021-04-10 18:10:08 -0300

    Create Delete and Show for Users

--- src/app/controllers/userController.ts
+++ src/app/controllers/userController.ts
@@ -1,9 +1,34 @@
-import { Request, Response } from 'express';
+import { request, Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import User from '../models/User';
 
 class UserController {
-  async store(request: Request, response: Response) {
+  public async index(request: Request, response: Response) {
+    const userRepository = getRepository(User);
+
+    const users = await userRepository.find();
+
+    if (!users) {
+      return response.sendStatus(406);
+    }
+
+    return response.send(users);
+  }
+
+  public async show(request: Request, response: Response) {
+    const userRepository = getRepository(User);
+    const { id } = request.params;
+
+    const user = await userRepository.findOne(id);
+
+    if (!user) {
+      return response.sendStatus(401);
+    }
+
+    return response.send(user);
+  }
+
+  public async store(request: Request, response: Response) {
     const repository = getRepository(User);
     const { name, email, password } = request.body;
 
@@ -18,6 +43,45 @@ class UserController {
       await repository.save(user);
 
       return response.send(user);
+    } catch {
+      return response.sendStatus(409);
+    }
+  }
+
+  public async update(request: Request, response: Response) {
+    const userRepository = getRepository(User);
+    const { name, email, password } = request.body;
+    const { id } = request.params;
+
+    const checkUserExist = await userRepository.findOne(id);
+
+    if (!checkUserExist) {
+      return response.sendStatus(401);
+    }
+
+    try {
+      userRepository.merge(checkUserExist, { name, email, password });
+      const newUser = await userRepository.save(checkUserExist);
+
+      return response.send(newUser);
+    } catch {
+      return response.sendStatus(403);
+    }
+  }
+
+  public async delete(request: Request, response: Response) {
+    const userRepository = getRepository(User);
+    const { id } = request.params;
+
+    const userExist = userRepository.findOne(id);
+
+    if (!userExist) {
+      return response.send(409);
+    }
+
+    try {
+      await userRepository.delete(id);
+      return response.send(200);
     } catch {
       return response.sendStatus(409);
     }
--- src/app/routes/users.routes.ts
+++ src/app/routes/users.routes.ts
@@ -3,6 +3,10 @@ import UserController from '../controllers/userController';
 
 const usersRouter = Router();
 
+usersRouter.get('/users', UserController.index);
+usersRouter.get('/users/:id', UserController.show);
 usersRouter.post('/users', UserController.store);
+usersRouter.put('/users/:id', UserController.update);
+usersRouter.delete('/users/:id', UserController.delete);
 
 export default usersRouter;
