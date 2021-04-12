7e071a95d821dcdea2dde0c66e12bb07927cd735
Warley Henrique <warley.henrique@gmail.com>
2021-04-11 23:46:43 -0300

    fix bugs members

--- src/app/controllers/memberController.ts
+++ src/app/controllers/memberController.ts
@@ -9,14 +9,23 @@ class MemberController {
     const members = await memberRepository.find();
 
     if (members.length === 0) {
-      return response.sendStatus(204);
+      return response.json({ return: 'there are no registered members' });
     }
 
     return response.send(members);
   }
 
   public async show(request: Request, response: Response) {
-    return response.json({ message: 'Im production' });
+    const memberRepository = getRepository(Member);
+    const { id } = request.params;
+
+    const member = await memberRepository.findOne(id);
+
+    if (!member) {
+      return response.status(404).json({ message: 'id not found' });
+    }
+
+    return response.send(member);
   }
 
   public async store(request: Request, response: Response) {
@@ -35,16 +44,50 @@ class MemberController {
       await memberRepository.save(newMember);
       return response.status(201).send(newMember);
     } catch (e) {
-      console.log(e);
       return response.status(409).json(e.message);
     }
   }
 
   public async update(request: Request, response: Response) {
-    return response.json({ message: 'Im production' });
+    const memberRepository = getRepository(Member);
+    const dataMember = request.body;
+    const { id } = request.params;
+
+    const member = await memberRepository.findOne(id);
+
+    if (!member) {
+      return response.status(404).json({ message: 'id not found' });
+    }
+
+    try {
+      memberRepository.merge(member, dataMember);
+      const newMember = await memberRepository.save(member);
+
+      return response.send(newMember);
+    } catch (e) {
+      return response.status(409).json(e.message);
+    }
   }
 
   public async delete(request: Request, response: Response) {
+    const memberRepository = getRepository(Member);
+    const { id } = request.params;
+
+    const memberExist = memberRepository.findOne(id);
+
+    if (!memberExist) {
+      return response.status(404).json({ message: 'id not found' });
+    }
+
+    try {
+      await memberRepository.delete(id);
+      const result = memberRepository.save;
+
+      return response.json;
+    } catch (err) {
+
+    }
+
     return response.json({ message: 'Im production' });
   }
 }
